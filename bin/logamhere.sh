f343ef237ea74c7811e188c33531e5dc3ef8986a
Information-Change J-C <infchg.appspot@gmail.com>
2019-03-08 11:12:13 +0000

    added a test

--- bin/logamhere.sh
+++ bin/logamhere.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-logger `hostname`  $SAT  `ip a` vxl   -n logger
+#logger `hostname`  $SAT  `ip a` vxl   -n logger
 
-logger `hostname`  $SAT  `ip a` vxl   -n mob
+logger `hostname`  test$SAT  `ip a` vxl   -n mob
 
 
