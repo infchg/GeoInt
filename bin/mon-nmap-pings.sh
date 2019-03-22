fd100914bd1485abec41766b96426496e555a009
infchg <infchg>
2019-03-22 12:16:12 +0000

    start&stop variables

--- bin/mon-nmap-pings.sh
+++ bin/mon-nmap-pings.sh
@@ -8,19 +8,28 @@ idx=${idx:-test}
 
 #CONFIG CAN USE THESE VARIABLES MANUALLY
 export DIR=/var/www/dokuwiki/syslog/  #demo only 
-export INFLUX='http://influ:8086/write?db=dba&precision=m'  # influx db in DNS
+export INFLUX='http://influ:8086/write?db=dba&precision=m'  # influx db in DNS ## mins
+export ELASTIC='http://starter-us-west-2-infra-1717073047.us-west-2.elb.amazonaws.com:9200/' #change with your elastic
 
 #OR CONFIG FILE, RECOMMENDED
 source /etc/measure/sping-conf/common.conf.sh
 
-#WHERE variable
+#WHERE variable and DAT file
 source /etc/measure/measure-pub/bin/location-hostname.sh 
+export dat=$DIR/raw-$idx-$snet.txt
 
 # Ping & Summarize in Line
  #start & end end=${end:-99}  # start & end , start=${start:-2}  #
-nmap -n -sP ${snet}${start:-2}-${end:-99} | egrep -e '\.[0-9]+\.'  -A1|perl -pe " s/.*for \d+\.\d+\.\d+\.(.*)\s$/${idx},r=${where},snet=${snet},i=\1 ip=\1/; s/.*\((.+)s lat.*/,lat=\1/" > $DIR/raw-$idx-$snet.txt
+nmap -n -sP ${snet}${start:-2}-${end:-99} | egrep -e '\.[0-9]+\.'  -A1|perl -pe " s/.*for \d+\.\d+\.\d+\.(.*)\s$/${idx},r=${where},snet=${snet},i=\1 ip=\1/; s/.*\((.+)s lat.*/,lat=\1/" > $dat   ##  DIR/raw-$idx-$snet.txt
 
-#send to Influx
-cat $DIR/raw-$idx-$snet.txt | curl -i -XPOST $INFLUX --data-binary @- ## mins
+#send to Influx     -> for Grafana
+		#cat $DIR/raw-$idx-$snet.txt | curl -i -XPOST $INFLUX --data-binary @- ## mins
+curl -i -XPOST $INFLUX --data-binary @$dat 
+
+#Send to Elastic bulk  -> for Kibana 
+perl -pe 's/^(\w+),/{"index":{"_index":"\1","_type":"_doc"}}\n{"/; s/ /,/;s/ /,time=/;s/\s*$/000}\n/;   s/,([\w\.]+)=/,"\1":/g; s/=/":/; s/:([\w\.]+),/:"\1",/g'   $dat  |curl -s -H 'Content-Type: Application/json'  -XPOST ${ELASTIC}/_bulk  --data-binary @-
+
+
+# perl -pe 's/^(\w+),/{"index":{"_index":"\1","_type":"_doc"}}\n{"/; s/ /,/;s/ /,time=/;s/\s*$/000}\n/;   s/,([\w\.]+)=/,"\1":/g; s/=/":/; s/:([\w+),/:"\1",/g'   $dat  |curl -s -H 'Content-Type: Application/json'  -XPOST ${ELASTIC}/_bulk  --data-binary @-
 
 
